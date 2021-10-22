04a2cd8adcd83da6aa203799a7553d5401a3ac2c
Chris Train <ctrain@pcs-publishing.com>
2021-10-22 17:10:51 +0100

    Add serverless error logging

--- src/util/handler.js
+++ src/util/handler.js
@@ -1,12 +1,18 @@
+
+import * as debug from './debug'
+
+
 export default function handler (lambda) {
   return async function (event, context) {
     let body, statusCode;
+
+    debug.init(event)
 
     try {
       body = await lambda(event, context)
       statusCode = 200
     } catch (e) {
-      console.error(e)
+      debug.flush(e)
       body = { error: e.message }
       statusCode = 500
     }
